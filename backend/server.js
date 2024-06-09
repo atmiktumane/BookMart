804739fb6120c75cb188fef159e1bbb4bd111dc7
atmiktumane <atmiktumane72@gmail.com>
2024-06-09 10:55:24 +0530

    MongoDb setup & connect express App to MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 // Load environment variables from .env file
 require("dotenv").config();
 
+// Import required packages
 const express = require("express");
+
+// Import custom connections
+const connectDB = require("./config/dbConnection");
+
+// Initialize the Express application
 const app = express();
 
-// Use the port specified in the environment variable, or default to 4004
+// Connect to MongoDB
+connectDB();
+
+// Define the port from environment variables or use default
 const port = process.env.PORT || 5000;
 
+// Start the Server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
