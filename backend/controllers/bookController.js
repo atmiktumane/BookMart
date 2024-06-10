6c3018b858db7f15b1d56599e32fed0293926612
atmiktumane <atmiktumane72@gmail.com>
2024-06-10 10:18:45 +0530

    Admin Role - Update an existing Book

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -6,8 +6,8 @@ const Book = require("../models/bookModel");
 //access Private -> Admin
 const addBook = asyncHandler(async (req, res) => {
   // Check whether user is admin or not -> using "ValidateTokenHandler" middleware ---> if user is admin then only add book, otherwise give error
-  const userr = req.user;
-  if (userr.role !== "admin") {
+  const user = req.user;
+  if (user.role !== "admin") {
     res.status(401);
     throw new Error("User does not have access to add book");
   }
@@ -35,4 +35,40 @@ const addBook = asyncHandler(async (req, res) => {
   res.status(201).json({ message: "Book added Successfully" });
 });
 
-module.exports = { addBook };
+//@desc Update the book -> admin
+//route PUT "/api/v1/update-book"
+//access Private -> Admin
+const updateBook = asyncHandler(async (req, res) => {
+  // Check whether user is admin or not -> using "ValidateTokenHandler" middleware ---> if user is admin then only update book, otherwise give error
+  const user = req.user;
+  if (user.role !== "admin") {
+    res.status(401);
+    throw new Error("User does not have access to add book");
+  }
+
+  const { bookid } = req.headers;
+
+  // validate request body/headers : check if bookid is received correctly or not
+  if (!bookid) {
+    res.status(400);
+    throw new Error("bookid is not received in headers");
+  }
+
+  // check if book is present in Database or not
+  const bookToBeUpdated = await Book.findById(bookid);
+  if (!bookToBeUpdated) {
+    res.status(400);
+    throw new Error("bookToBeUpdated is not present in Database");
+  }
+
+  // Update Book
+  const updatedBook = await Book.findByIdAndUpdate(bookid, req.body, {
+    new: true,
+  });
+
+  res
+    .status(200)
+    .json({ message: "Book Updated Successfully", updateData: updatedBook });
+});
+
+module.exports = { addBook, updateBook };
--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const authenticateToken = require("../middleware/validateTokenHandler");
-const { addBook } = require("../controllers/bookController");
+const { addBook, updateBook } = require("../controllers/bookController");
 
 const router = express.Router();
 
 // add-book -> admin
 router.post("/add-book", authenticateToken, addBook);
+
+// update book -> admin
+router.put("/update-book", authenticateToken, updateBook);
 
 module.exports = router;
