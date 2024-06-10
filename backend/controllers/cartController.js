ccf1b86d222b8334be625699b631828bd4cb7f4a
atmiktumane <atmiktumane72@gmail.com>
2024-06-10 19:30:57 +0530

    Cart Functionality - Remove book from cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -44,4 +44,48 @@ const addBookInCart = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Book is added to Cart" });
 });
 
-module.exports = { addBookInCart };
+//@desc Remove Book from Cart
+//route PUT "/api/v1/remove-book-from-cart/:id"
+//access Private
+const removeBookFromCart = asyncHandler(async (req, res) => {
+  // get authorized user from Token ->  getting from middleware validateTokenHandler
+  const authorizedUser = req.user;
+  const { bookid } = req.params;
+
+  const { id } = req.headers;
+
+  // Validate request body
+  if (!id || !bookid) {
+    res.status(400);
+    throw new Error("UserId and BookId are required");
+  }
+
+  const userData = await User.findById(id);
+
+  // Verify whether user is authorized to add book to Carts or not because user cannot update Cart list of other users
+  if (authorizedUser.name !== userData.username) {
+    res.status(401);
+    throw new Error("User is not Authorized to add book to Cart");
+  }
+
+  // check if User is present in Database or not
+  if (!userData) {
+    res.status(404);
+    throw new Error("User is not found in database");
+  }
+
+  const isBookInCart = userData.cart.includes(bookid);
+
+  // check if Book is present in User's Cart
+  if (!isBookInCart) {
+    res.status(400);
+    throw new Error("Book is not present in Cart");
+  }
+
+  // all good, remove Book from Cart
+  await User.findByIdAndUpdate(id, { $pull: { cart: bookid } });
+
+  res.status(200).json({ message: "Book is removed from Cart" });
+});
+
+module.exports = { addBookInCart, removeBookFromCart };
--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const authenticateToken = require("../middleware/validateTokenHandler");
-const { addBookInCart } = require("../controllers/cartController");
+const {
+  addBookInCart,
+  removeBookFromCart,
+} = require("../controllers/cartController");
 
 const router = express.Router();
 
 // add book in Cart -> "PUT" method because in userModel we are updating the Cart array
 router.put("/add-book-in-cart", authenticateToken, addBookInCart);
+
+// Remove Book from User's Cart -> "PUT" method because in userModel we are updating the Cart array
+router.put(
+  "/remove-book-from-cart/:bookid",
+  authenticateToken,
+  removeBookFromCart
+);
 
 module.exports = router;
