72fde870c300a299aba6245e9c7e08f6bad5bdb0
atmiktumane <atmiktumane72@gmail.com>
2024-06-25 13:02:00 +0530

    Updated CSS for hero section

--- backend/frontend/src/components/Hero.jsx
+++ backend/frontend/src/components/Hero.jsx
@@ -7,23 +7,24 @@ export const Hero = () => {
     <div className="md:h-[75vh] flex flex-col md:flex-row">
       {/* Column 1 - Content */}
       <div className="w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center">
+        {/* Intro Tag */}
         <span className="text-4xl lg:text-6xl font-semibold text-purple-300 text-center lg:text-left">
-          Discover Your Next Great Read
+          Explore Your Next Great Read
         </span>
 
+        {/* Paragraph */}
         <p className="mt-4 text-xl text-zinc-300 text-center lg:text-left">
-          Uncover captivating stories, enriching knowledge, and endless
-          inspiration in our curated collection of books
+          Passionate about discovering and sharing great reads. Explore new
+          books to find hidden gems.
         </p>
 
-        <div className="mt-8">
-          <Link
-            to="/all-books"
-            className="text-purple-300 text-xl lg:text-2xl font-semibold border border-purple-300 px-10 py-2 rounded-full hover:bg-zinc-800"
-          >
-            Discover Books
-          </Link>
-        </div>
+        {/* Button */}
+        <Link
+          to="/all-books"
+          className="mt-8 text-purple-300 text-xl lg:text-2xl font-semibold border border-purple-300 px-10 py-2 rounded-full hover:bg-zinc-800"
+        >
+          Discover Books
+        </Link>
       </div>
 
       {/* Column 2 - Image */}
